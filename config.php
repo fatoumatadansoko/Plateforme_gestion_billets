67a36b1cf4a811c46aa98f8f14dd2280ea12d36f
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-03-06 13:27:08 +0000

    on retrouve ici le fichier config pour connecter à la base de données la page

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php 
+define("DBHOST", "Localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DBNAME", "Traitement_billet");
+
+//create connexion
+$conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
+
+
